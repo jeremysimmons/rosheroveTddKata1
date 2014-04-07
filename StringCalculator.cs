ca022cc18d42ead3d6f3f46d017eafe39d120281
jeremy simmons <jsimmons@jeremysimmons.net>
2014-04-07 13:14:19 -0600

    feat(test): implemented kata

---
+++ StringCalculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TddKata1
+{
+    class StringCalculator
+    {
+        private List<ProcessValueAction> _numberStrageies;
+
+        private delegate void ProcessValueAction(int newNumber, ref int aggregate, ref bool stopProcessing);
+
+        public StringCalculator()
+        {
+            _numberStrageies = new List<ProcessValueAction>();
+            _numberStrageies.Add(ThrowOnNegativeValues);
+            _numberStrageies.Add(SkipNumbersLargerThan1000);
+            _numberStrageies.Add(AggregateNewNumber);
+        }
+
+        private static void AggregateNewNumber(int newNumber, ref int aggregate, ref bool stopProcessing)
+        {
+            aggregate += newNumber;
+        }
+
+        private static void SkipNumbersLargerThan1000(int newNumber, ref int aggregate, ref bool stopProcessing)
+        {
+            if (newNumber > 1000)
+                stopProcessing = true;
+        }
+
+        private static void ThrowOnNegativeValues(int newNumber, ref int aggregate, ref bool stopProcessing)
+        {
+            if (newNumber < 0)
+            {
+                throw new ArgumentOutOfRangeException("negatives not allowed");
+            }
+        }
+
+        public int Add(string inputs)
+        {
+            CalculationExpressionParser parser = new CalculationExpressionParser();
+            string[] delimiter = parser.Delimiters(inputs);
+            string[] allSeparators = delimiter.Concat(new[]{"\n"}).ToArray();
+            string[] strings = inputs.Split(allSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+            int temp = 0;
+            int result = 0;
+            foreach (string number in strings)
+            {
+                if(Int32.TryParse(number, out temp))
+                {
+                    bool stopProcessing = false;
+                    foreach (ProcessValueAction numberStragey in _numberStrageies)
+                    {
+                        numberStragey(temp, ref result, ref stopProcessing);
+                        if (stopProcessing)
+                            break;
+                    }    
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ StringCalculatorTest.cs
@@ -0,0 +1,102 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddKata1
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        private readonly CalculationExpressionParser _calculationExpressionParser = new CalculationExpressionParser();
+
+        [TestMethod]
+        public void Add_SumsEmptyString()
+        {
+            StringCalculator calc = new StringCalculator();
+            int result = calc.Add("");
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Add_SumsSingleNumber()
+        {
+            StringCalculator calc = new StringCalculator();
+            int result = calc.Add("1");
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Add_SumsTwoNumbers_SeparatedByComma()
+        {
+            StringCalculator calc = new StringCalculator();
+            int result = calc.Add("1,2");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Add_SumsMultipleNumbers_SeparatedByComma()
+        {
+            StringCalculator calc = new StringCalculator();
+            int result = calc.Add("1,2,3,4");
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void Add_Allows_Comma_Or_Newline_Delimiters()
+        {
+            StringCalculator calc = new StringCalculator();
+            int result = calc.Add("1\n2,3");
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Add_DefaultDelimiterSpecifiedFirstLine()
+        {
+            StringCalculator calc = new StringCalculator();
+            int result = calc.Add("//;\n1;2");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Add_With_Negative_Number_Throws_Exception()
+        {
+            StringCalculator calc = new StringCalculator();
+            int result = calc.Add("-1");
+        }
+
+        [TestMethod]
+        public void Add_Ingores_Numbers_Larger_Than_1000()
+        {
+            StringCalculator calc = new StringCalculator();
+            int result = calc.Add("2,1001");
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Add_Handles_Multi_Character_Delimiter()
+        {
+            string calcProgram = "//[***]\n1***2***3";
+            StringCalculator calc = new StringCalculator();
+            int result = calc.Add(calcProgram);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Add_Handles_Multiple_Single_Character_Delimiters()
+        {
+            string calcProgram = "//[*][%]\n1*2%3";
+            StringCalculator calc = new StringCalculator();
+            int result = calc.Add(calcProgram);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Add_Handles_Multiple_Multi_Character_Delimiters()
+        {
+            string calcProgram = "//[***][%%%]\n1***2%%%3";
+            StringCalculator calc = new StringCalculator();
+            int result = calc.Add(calcProgram);
+            Assert.AreEqual(6, result);
+        }
+    }
+}
